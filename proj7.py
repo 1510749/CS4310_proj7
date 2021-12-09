c7e13d77d5f31bb2133a4d969cafa3563712f7cc
JasonC <31429765+1510749@users.noreply.github.com>
2021-12-08 20:08:12 -0800

    Solved race conditions with semaphores
    Programs & threads forcefully exits after 10 seconds

--- proj7.py
+++ proj7.py
@@ -1,6 +1,7 @@
 import threading
 import time
 import random
+import sys
 
 n = 100
 buffer = [0 for x in range(n)]
@@ -10,47 +11,64 @@ race_condition = "Race Condition Occurred"
 def producer():
     next_in = 0
     while(1):
-        k1 = random.randint(1,99)
         
+        k1 = random.randint(1,99)
         print("k1: " + str(k1))
         for j in range(k1):
+            semae.acquire()
             buffer[(next_in + j) % n] += 1
-            if(buffer[(next_in + j) % n] >= 3): #IF value reaches 3, just exit because race condition will already have occurred
+            if(buffer[(next_in + j) % n] >= 3): # IF value reaches 3, just exit because race condition will already have occurred
                 return True
+            semaf.release()
         next_in = (next_in + k1) % n
         print("PRODUCED: " + str(buffer))
-        t1 = random.randrange(20,99) * .1  #pauses for 2.0-9.9 seconds
-        print("TIME1: " + str(t1) + "s" )
-        time.sleep(t1)
-
         
+        #t1 = random.randrange(20,99) * .1   # Pauses for 2.0-9.9 seconds
+        #print("TIME1: " + str(t1) + "s" )
+        #time.sleep(t1)
+
 
 
 def consumer():
     next_out = 0
     while(1):
-        t2 = random.randrange(20, 99) * .1 #pauses for 2.0-9.9 seconds
-        print("TIME2: " + str(t2) + "s" )
-        time.sleep(t2)
+        #t2 = random.randrange(20, 99) * .1  # Pauses for 2.0-9.9 seconds
+        #print("TIME2: " + str(t2) + "s" )
+        #time.sleep(t2)
+        
         k2 = random.randint(1,99)
         print("k2: " + str(k2))
         for k in range(k2):
+            semaf.acquire()
             data = buffer[(next_out + k) % n]
-            if (data > 1): # race condition detected; exit
+            if (data > 1): # Race condition detected; exit
                 print(race_condition)
                 print("IF: " + str(buffer))
                 return True
             else:
                 buffer[(next_out + k) % n] = 0
+            semae.release()
         next_out = (next_out + k2) % n
         print("CONSUMED: " + str(buffer))
-            
+        
+
+        
 
 
 try:
-   thread1 = threading.Thread(target = producer)
-   thread2 = threading.Thread(target = consumer)
-   thread1.start()
-   thread2.start()
+    start = time.time()
+    end = start + 10  # Force close after 10 seconds
+    semaf = threading.Semaphore(0)
+    semae = threading.Semaphore(n)
+    thread1 = threading.Thread(target = producer)
+    thread2 = threading.Thread(target = consumer)
+    thread1.daemon = True # Make threads Daemons so they exit with the program
+    thread2.daemon = True
+    thread1.start()
+    thread2.start()
+
+
+    while(time.time() <= end):
+        running = True # Just to have the program and threads only run for the duration of the timer
 except:
    print("Error: unable to start thread")
